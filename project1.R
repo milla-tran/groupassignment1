d5267a7c09e6c1ef080cb272e04bcb6741b509d4
alissagordon <99290922+alissagordon@users.noreply.github.com>
2022-02-08 18:42:07 -0500

    Collab 2 Update
    Filtered the tourism data set and stored a plot for it under output$plot

--- project1.R
+++ project1.R
@@ -1,11 +1,14 @@
 #group project 1
 install.packages("shiny")
+install.packages("fpp3")
 library(shiny)
-
+library(fpp3)
+data("tourism")
 ui <- fluidPage("Hello world!")
 
 server <- function(input, output){
-  
+   new<-filter(tourism, tourism$Region=="Adelaide")
+  output$plot<-renderPlot((autoplot(new)))
 }
   
 shinyApp(ui = ui, server=server) 
